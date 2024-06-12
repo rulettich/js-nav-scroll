fbf4011df848718fdb4c13a2c4be484183d29c92
rulettich <rulettich@gmail.com>
2024-06-12 20:43:04 +0300

    nav & scroll

--- src/route/index.js
+++ src/route/index.js
@@ -6,8 +6,13 @@ const router = express.Router()
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
 
+const nav = require('./nav')
+const scroll = require('./scroll')
+
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/nav', nav)
+router.use('/scroll', scroll)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
